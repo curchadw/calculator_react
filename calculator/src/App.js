88d59ffbe4f56a40e19bf55e3af1ce7b5fb1f779
curtis <laurencechadwell@gmail.com>
2021-07-03 16:32:37 -0500

    Need to correct errors in App.js

---
+++ calculator/src/App.js
@@ -0,0 +1,53 @@
+
+import './App.css';
+import Results from './components/Results'
+import KeyPadComponent from './components/KeyPadComponent';
+import React, {useEffect, useState} from 'react';
+
+
+function App() {
+
+  const[result, setResult] = useState(0)
+
+  calculate =()=>{
+    try {
+      setResult((eval(result) || "") + "")
+    }catch(e){
+      setResult("error")
+    }
+  }
+
+  reset =()=>{
+    setResult("")
+  }
+
+  backspace =()=>{
+    setResult(result.slice(0,-1))
+  }
+
+  onClick = button =>{
+    if(button === '='){
+      calculate()
+    }else if(button === 'C'){
+      reset()
+    }else if(button === 'CE'){
+      backspace()
+    }else{
+      setResult(result + button)
+    }
+  }
+
+  
+
+  return (
+    <div>
+      <div className="calculator-body">
+        <h1>Calculator With Hooks</h1>
+        <Results result={useState(result)}/>
+        <KeyPadComponent onClick={() => setResult(result)}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator/src/components/Results.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Result = (props) =>{
+
+    
+
+    return(
+
+
+        <div className = 'result'>
+            <p>{props.result}</p>
+        </div>
+    )
+
+}
+
+export default Result
